0757a66de417c65ce5d5c1cd837eed430c55d4f8
je94castillo <je94castillo@gmail.com>
2023-03-29 17:48:14 -0500

    se agregan cambios

--- app.js
+++ app.js
@@ -10,8 +10,101 @@ switcher.addEventListener('click', function() {
     if(className == "light-theme") {
         this.textContent = "Dark";
     } else {
-        this.textContent = "Light";
+        this.textContent = "Light" ;
     }
 
     console.log('current class name: ' + className);
 });
+
+let loadMoreBtn = document.querySelector('#load-more');
+let currentItem = 4;
+
+loadMoreBtn.onclick = ()=>{
+
+    let boxes = [...document.querySelectorAll('.box-container .box')];
+    for(var i = currentItem; i< currentItem + 4; i++){
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem += 4;
+    if(currentItem >= boxes.length){
+        loadMoreBtn.style.display ='none';
+    }
+}
+
+//carrito
+
+const carrito = document.getElementById('carrito');
+const elementos1 = document.getElementById('lista-1');
+const lista = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito1');
+
+cargarEventListeners();
+
+function cargarEventListeners(){
+    elementos1.addEventListener('click', comprarElemento);
+    carrito.addEventListener('click', eliminarElemento);
+    vaciarCarritoBtn.addEventListener('click', vaciarcarrito);
+}
+
+function comprarElemento(e){
+    e.prevetnDefault();
+
+    if(e.target.classList.contains('agregar-carrito btn-3')){
+        const elemento = e.target.parentElement.parentElement;
+        leerDatosElemento(elemento);
+    }
+}
+
+function leerDatosElemento(elemento){
+    const infoElemento = {
+        imagen: elemento.querySelector('img').src,
+        titulo: elemento.querySelector('h3').textContent,
+        precio: elemento.querySelector('.precio').textContent,
+        id: elemento.querySelector('a').getAttribute('data-id')
+    }
+    insertarCarrito(infoElemento)
+}
+
+function insertarCarrito(elemento){
+    const row = document.createElemento('tr');
+    row.innerHTML =`
+    
+        <td>
+            <img src = "${elemento.imagen}" width=100 />
+        </td>
+
+        <td>
+          ${elemento.titulo}
+        </td>
+
+        <td>
+           ${elemento.precio}
+        </td>
+
+        <td>
+            <a href="#" class = "borrar" data-id = "${elemento.id}" > X </a>
+        </td>
+    `;
+    
+    
+    lista.appendChild(row);
+}
+
+function eliminarElemento(e){
+    e.prevetnDefault();
+    let elemento,
+        elementoId;
+
+    if(e.target.classList.contains('borrar')){
+        e.target.parentElement.parentElement.remove();
+        elemento = e.target.parentElement.parentElement;
+        elementoId = elemento.querySelector('a').getAttribute('data-id');
+    }
+}
+
+function vaciarcarrito(){
+    while(lista.firstChild){
+        lista.removeChild(lista.firstChild);
+    }
+    return false;
+}
